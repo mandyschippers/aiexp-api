16ab62871c421148ea59213cf17de7765927d1fa
Mandy Schippers <mandyschippers@192-168-1-139.tpgi.com.au>
2024-05-03 16:38:12 +1000

    backend for send message

--- api/resources.py
+++ api/resources.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restx import Resource
 from . import api
 
@@ -5,3 +6,10 @@ from . import api
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'from blueprint!'}
+    
+@api.route('/message', methods=['POST'])
+class GetMessage(Resource):
+    def post(self):
+        data = request.get_json()
+        #now we can add conversation logic with langchain and llm API
+        return {'received message': data.get('message', 'no message was provided')}
